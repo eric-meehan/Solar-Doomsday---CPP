c5f0c66f845a0843c8985a96a4962e47b42687f2
Eric Meehan <eric.meehan@icloud.com>
2020-11-23 15:33:40 -0500

    Implemented a solution that utilizes vectors

--- Solar Doomsday - CPP/main.cpp
+++ Solar Doomsday - CPP/main.cpp
@@ -6,9 +6,24 @@
  */
 
 #include <iostream>
+#include <math.h>
+#include <vector>
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::vector<int> Results;
+    int TotalArea = 15324;
+    double Panel;
+    while (TotalArea > 0)
+    {
+        Panel = floor(sqrt(TotalArea));
+        TotalArea -= pow(Panel, 2);
+        Results.push_back(pow(Panel, 2));
+    }
+    for (auto each : Results)
+    {
+        std::cout << each << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
